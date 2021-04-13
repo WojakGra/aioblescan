5cd8e3960c1d19238f6726eb63cdadb8ef29f6d7
Wojak <39022177+WojakGra@users.noreply.github.com>
2021-04-13 04:56:51 +0200

    Update __init__.py

--- aioblescan/plugins/__init__.py
+++ aioblescan/plugins/__init__.py
@@ -2,4 +2,4 @@ from .eddystone import EddyStone
 from .ruuviweather import RuuviWeather
 from .bluemaestro import BlueMaestro
 from .atcmithermometer import ATCMiThermometer
-from .INode4 import INode4
+from .inode4 import INode4
